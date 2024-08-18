ebbe64e369ffffa96e4723a01a2e44007d68d1a8
jgpentz <jpentz1@gmail.com>
2024-08-18 01:18:27 -0600

    Book version of parsing function literals

--- parser/parser.go
+++ parser/parser.go
@@ -350,34 +350,55 @@ func (p *Parser) parseIfExpression() ast.Expression {
 	return expression
 }
 
+func (p *Parser) parseFunctionParameters() []*ast.Identifier {
+	identifiers := []*ast.Identifier{}
+
+	if p.peekTokenIs(token.RPAREN) {
+		p.nextToken()
+		return identifiers
+	}
+
+	p.nextToken()
+
+	ident := &ast.Identifier{
+		Token: p.curToken,
+		Value: p.curToken.Literal,
+	}
+	identifiers = append(identifiers, ident)
+
+	for p.peekTokenIs(token.COMMA) {
+		p.nextToken()
+		p.nextToken()
+
+		ident := &ast.Identifier{
+			Token: p.curToken,
+			Value: p.curToken.Literal,
+		}
+		identifiers = append(identifiers, ident)
+	}
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return identifiers
+}
+
 func (p *Parser) parseFunctionLiteral() ast.Expression {
-	expression := &ast.FunctionLiteral{Token: p.curToken}
+	lit := &ast.FunctionLiteral{Token: p.curToken}
 
 	if !p.expectPeek(token.LPAREN) {
 		return nil
 	}
 
-	p.nextToken()
+    lit.Parameters = p.parseFunctionParameters()
 
-	// Parse params
-	for p.curToken.Literal != token.RPAREN {
-		if p.curToken.Literal != token.COMMA {
-			ident := &ast.Identifier{
-				Token: p.curToken,
-				Value: p.curToken.Literal,
-            }
-			expression.Parameters = append(expression.Parameters, ident)
-		}
-
-		p.nextToken()
+	if !p.expectPeek(token.LBRACE) {
+		return nil
 	}
 
-    if !p.expectPeek(token.LBRACE){
-        return nil
-    }
-
 	// Expect block statement
-	expression.Body = p.parseBlockStatement()
+	lit.Body = p.parseBlockStatement()
 
-	return expression
+	return lit
 }
