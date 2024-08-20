3cbbe51ba452aa637edba9091dc8951805afe82b
jgpentz <jpentz1@gmail.com>
2024-08-20 01:58:34 -0600

    Added preface to error messages

--- repl/repl.go
+++ repl/repl.go
@@ -36,6 +36,8 @@ func Start(in io.Reader, out io.Writer) {
 }
 
 func printParserErrors(out io.Writer, errors []string) {
+    io.WriteString(out, "Whoops! We ran into some monkey business here!\n")
+    io.WriteString(out, " parser errors: \n")
     for _, msg := range errors {
         io.WriteString(out, "\t"+msg+"\n")
     }
